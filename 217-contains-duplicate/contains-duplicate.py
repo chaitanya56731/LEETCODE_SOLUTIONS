70fa68527893f59d9c726392a04d2938f23698c7
chaitanya56731 <127004402+chaitanya56731@users.noreply.github.com>
2025-06-27 00:13:12 +0530

    Time: 19 ms (59.47%) | Memory: 25.9 MB (46.03%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        see = set()
+        for num in nums:
+            if num in see:
+                return True
+            see.add(num)
+        return False
+        
