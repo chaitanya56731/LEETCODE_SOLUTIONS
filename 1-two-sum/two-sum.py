44f85c851e4e42eba2cfef3a17bcfbb3ca98df36
chaitanya56731 <127004402+chaitanya56731@users.noreply.github.com>
2025-09-14 22:49:29 +0530

    Time: 4 ms (53.24%) | Memory: 13 MB (90.30%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def twoSum(self,nums,target):
-        seen = {}                      # value -> index
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in seen:
-                return [seen[diff], i]
-            seen[num] = i
+        dic = {}
+        for i,num in enumerate(nums):
+            complement = target - num
+            if complement in dic:
+                return [dic[complement],i]
+            dic[num] = i
+        return [] 
         
