12ce7e613262c0e3332c43f0eacd31aeeec28188
chaitanya56731 <127004402+chaitanya56731@users.noreply.github.com>
2025-09-25 23:50:22 +0530

    Time: 5 ms (31.55%) | Memory: 12.5 MB (51.39%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,10 +1,7 @@
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        sett =set()
-        while n != 1:
-            sett.add(n)
-            n= sum([int(x)**2 for x in str(n)])
-            if n in sett:
-                return False
-        return True
-        
+class Solution(object):
+    def isHappy(self, n):
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit)**2 for digit in str(n))
+        return n == 1
