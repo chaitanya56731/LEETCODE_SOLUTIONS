73c5d4e7b9e29a39fe2ad94f4b0fc925c3c2e573
chaitanya56731 <127004402+chaitanya56731@users.noreply.github.com>
2025-09-26 23:51:12 +0530

    Time: 27 ms (59.80%) | Memory: 17.9 MB (20.81%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        total = 0
+        for x in range(1,n+1):
+            if x % 3 == 0 or x % 5 == 0 or x % 7 == 0:
+                total += x
+        return total
+        
