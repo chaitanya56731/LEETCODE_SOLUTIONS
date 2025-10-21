7c4184080a6ebfa7e11dbdb568a48f0aef64ce75
chaitanya56731 <127004402+chaitanya56731@users.noreply.github.com>
2025-10-21 12:10:11 +0530

    Time: 7 ms (96.70%) | Memory: 18.4 MB (36.18%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,15 +1,16 @@
-class Solution(object):
-    def solveNQueens(self, n):
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
         def dfs(qu,xy_diff,xy_sum):
-            p=len(qu)
+            p = len(qu)
             if p == n:
                 result.append(qu)
                 return None
             for q in range(n):
                 if q not in qu and p-q not in xy_diff and p+q not in xy_sum:
                     dfs(qu+[q],xy_diff+[p-q],xy_sum+[p+q])
-        result =[]
+        result = []
         dfs([],[],[])
-        return[["."*i+"Q"+"."*(n-i-1) for i in sol]for sol in result]
+        return [["."*i+"Q"+"."*(n-i-1)for i in sol] for sol in result]
         
+
         
