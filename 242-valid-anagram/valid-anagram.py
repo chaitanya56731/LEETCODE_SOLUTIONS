81b98e625c840a65ed757546116b23f2e73be8b9
chaitanya56731 <127004402+chaitanya56731@users.noreply.github.com>
2025-10-28 19:06:47 +0530

    Time: 10 ms (74.61%) | Memory: 17.8 MB (86.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,11 @@
-from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        count = [0] * 26
+        for c1,c2 in zip(s,t):
+            count[ord(c1)-ord('a')] += 1
+            count[ord(c2)-ord('a')] -= 1
+        return all(x==0 for x in count)
+        
         
